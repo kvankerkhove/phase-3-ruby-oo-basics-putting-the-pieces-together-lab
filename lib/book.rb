a9cd8c04b009a4e73575c14dbbe57a1ae1ef276a
Katey VanKerkhove <kateyvankerkhove1@gmail.com>
2022-05-16 21:41:16 -0400

    commit tree c01a249bd20e0fed887a7b98d84a23266f118959

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,21 @@
+class Book
+    attr_reader :title
+    attr_accessor :author
+    attr_accessor :page_count
+    attr_accessor :genre
+
+    def initialize(title)
+        @title = title
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
 
 
+end
+
+b1 = Book.new("And Then There Were None")
+b1.author = "Agatha Christie"
+b1.page_count = 272
+b1.genre = "Mystery"
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,26 @@
 # Make your shoe class here!
+class Shoe
+    attr_reader :brand
+
+    attr_accessor :color
+    attr_accessor :size
+    attr_accessor :material
+    attr_accessor :condition
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+end
+
+s1 = Shoe.new("Adidas")
+
+shoe = Shoe.new("Nike")
+shoe.color = "red"
+shoe.size = 9.5
+shoe.material = "suede"
+shoe.condition = "tattered"
